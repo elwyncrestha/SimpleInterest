00ab5dc7c63382a070e31d4622e2be4b3502de90
Elvin Shrestha <elwyncrestha@gmail.com>
2019-11-13 11:15:59 +0545

    Add SimpleInterestActivity Navigation

--- app/src/main/java/com/elvin/simpleinterest/MainActivity.java
+++ app/src/main/java/com/elvin/simpleinterest/MainActivity.java
@@ -1,79 +1,37 @@
 package com.elvin.simpleinterest;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.elvin.simpleinterest.model.SimpleInterest;
+import com.elvin.simpleinterest.activity.SimpleInterestActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etPrincipal;
-    EditText etTime;
-    EditText etRate;
-    Button btnCalculate;
-    TextView tvSimpleInterest;
+    Button btnSimpleInterest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bindControls();
+        bindControl();
         configureListeners();
     }
 
-    private void bindControls() {
-        this.etPrincipal = findViewById(R.id.etPrincipal);
-        this.etTime = findViewById(R.id.etTime);
-        this.etRate = findViewById(R.id.etRate);
-        this.btnCalculate = findViewById(R.id.btnCalculate);
-        this.tvSimpleInterest = findViewById(R.id.tvSimpleInterest);
+    private void bindControl() {
+        this.btnSimpleInterest = findViewById(R.id.btnSimpleInterest);
     }
 
     private void configureListeners() {
-        this.btnCalculate.setOnClickListener(new View.OnClickListener() {
+        this.btnSimpleInterest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (TextUtils.isEmpty(etPrincipal.getText())) {
-                    etPrincipal.setError("Principal is empty");
-                    return;
-                }
-                if (TextUtils.isEmpty(etTime.getText())) {
-                    etPrincipal.setError("Time is empty");
-                    return;
-                }
-                if (TextUtils.isEmpty(etRate.getText())) {
-                    etPrincipal.setError("Rate is empty");
-                    return;
-                }
-
-                float principal, time, rate;
-                principal = time = rate = 0.0f;
-                try {
-                    principal = Float.parseFloat(etPrincipal.getText().toString());
-                } catch (Exception e) {
-                    etPrincipal.setError("Enter valid principal");
-                    return;
-                }
-                try {
-                    time = Float.parseFloat(etTime.getText().toString());
-                } catch (Exception e) {
-                    etPrincipal.setError("Enter valid time");
-                    return;
-                }
-                try {
-                    rate = Float.parseFloat(etRate.getText().toString());
-                } catch (Exception e) {
-                    etRate.setError("Enter valid rate");
-                    return;
-                }
-                tvSimpleInterest.setText(String.valueOf(new SimpleInterest(principal, time, rate).calculate()));
+                Intent intent = new Intent(MainActivity.this, SimpleInterestActivity.class);
+                startActivity(intent);
             }
         });
     }
